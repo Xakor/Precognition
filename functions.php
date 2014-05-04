d24617d6db3e8545a140ba1559e18f0fb409bf27
Xakor <fha.post@gmail.com>
2014-05-04 14:24:45 +0200

    typos

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 
 <?php
 // Create nav menu
-if(function_exists('reguster_nav_menus')) {
+if(function_exists('register_nav_menus')) {
 	register_nav_menus ( array( 'primary' => 'Header Navigation' ));
 }
 
