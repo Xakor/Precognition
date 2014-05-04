0978283bcf2b39c38c2313a8fce68f67fbb9943c
Xakor <fha.post@gmail.com>
2014-05-04 16:43:10 +0200

    Cleaned up header
    Lost old format

--- header.php
+++ header.php
@@ -13,32 +13,16 @@
 
     </head>
 	<body>
-
-    <!-- old header element 
-        <div class="container">
-            <header>
-                <div class="five columns clearfix">
-                    <a href="<?php get_option ('home'); ?>"><img src="<?php bloginfo('template_url'); ?>/img/logo.jpg" title="<?php bloginfo('title'); ?>"></a>
-                </div>
-                <div class="sixteen columns nav-bar">
-                        <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-
-                </div>
-            </header>
-        </div>
-    -->
-
-        <div class="container_12">
+        <div class="container_12" head_banner_area>
             <a href="<?php get_option ('home'); ?>"><img src="<?php bloginfo('template_url'); ?>/img/logo_temp.png" title="<?php bloginfo('title'); ?>"></a>
         </div>
         <div class="container_12">
-            <div class="grid_4 red">
+            <div class="grid_4 red head_search_area">
                 Search Bar
             </div>
-            <div class="grid_8 red">
+            <div class="grid_8 red head_menu_area">
                 <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
             </div>
         </div>
-            
     </body>
 </html>
