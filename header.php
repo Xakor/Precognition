77796721e108efadeef2e4d1fb0d9ffd34dc241b
Xakor <fha.post@gmail.com>
2014-05-04 13:54:23 +0200

    master

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    	<title>
+			<?php 
+                wp_title(' | ', 'true', 'right');
+				bloginfo( 'name' ); 
+            ?>
+        </title>
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url');?>/style.css">
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url');?>/skeleton.css">
+        <?php wp_head(); ?>
+        
+    </head>
+	<body>
+        <div class="container">
+        	<div class="five columns">
+            	<a href="<?php get_option ('home'); ?><img src="">
+            </div>
+        </div>
+    </body>
+</html>
