4fe141109c6fc638ae76bef6bcc43a269f4190ce
Sommer <sommerawesome@gmail.com>
2014-05-04 21:06:01 +0200

    Box-size neatness fixed (Read Notes!)
    Made sure that the "visual" boxes matches the "invisible" boxes. Added
    id's for widgets and content-entities, to enable the possibility of
    styling these further.
    
    CHANGED CLASS .shadow:
    Changed the shadow temporarily (?) to only have a shadow on the bottom
    of a box.

--- index.php
+++ index.php
@@ -1,31 +1,33 @@
 <?php get_header(); ?>
 <div class="container_12">
-    <div class="grid_4 floatleft">
+    <div id="widget" class="grid_4 floatleft shadow">
         <?php get_sidebar(); ?>
     </div>
-    <?php
-        if ( have_posts() ) {
-            while ( have_posts() ) {
-                the_post(); ?>
+    <div id="content" class="grid_8 floatright shadow">
+        <?php
+            if ( have_posts() ) {
+                while ( have_posts() ) {
+                    the_post(); ?>
 
-                <div id="content" class="grid_8 floatright shadow">
-                    <div class="grid_8">
-                        <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+                    <div  id="content_entity" class="grid_8 padding boxsizing">
+                        <div id="content_title">
+                            <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+                        </div>
+                        <div id="content_date">
+                            <small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
+                        </div>
+                        <div id="content_text">
+                            <?php the_content(); ?>
+                        </div>
+                        <div id="content_metadata">
+                            <p class="postmetadata">Posted in <?php the_category(', '); ?></p>
+                        </div>
                     </div>
-                    <div class="grid_8">
-                        <small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
-                    </div>
-                    <div class="grid_8">
-                        <?php the_content(); ?>
-                    </div>
-                    <div class="grid_8">
-                        <p class="postmetadata">Posted in <?php the_category(', '); ?></p>
-                    </div>
-                </div>
 
-                <?php
-            } // end while
-        } // end if
-    ?>
+                    <?php
+                } // end while
+            } // end if
+        ?>
+    </div>
 </div>
 <?php get_footer(); ?>
