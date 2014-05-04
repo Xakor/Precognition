5291bfb5d7ed42593e1e4cfb89cd6c43901566bf
Sommer <sommerawesome@gmail.com>
2014-05-04 15:04:44 +0200

    Backtracking
    Using newer wordpress expressions from now on.

--- footer.php
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript" src="<?php bloginfo( 'template_url' );?>/js/jquery.flexslider.js" ></script>
-<script type="text/javascript">
-    // Can also be used with $(document).ready()
-    jQuery(window).load(function() {
-      jQuery('.flexslider').flexslider({
-        animation: "slide"
-      });
-    });
-</script>
--- functions.php
+++ functions.php
@@ -6,5 +6,44 @@
             )
         );
     }
+
+    function custom_enqueue_scripts() {
+
+        //* Load scripts only if custom background is being used
+        if ( ! get_background_image() )
+            return;
+
+        //* Load jQuery
+        wp_enqueue_script("jquery");
+
+        wp_enqueue_script( 'custom-backstretch', get_bloginfo( 'stylesheet_directory' ) . '/js/backstretch.js', array( 'jquery' ), '1.0.0' );
+        wp_enqueue_script( 'custom-backstretch-set', get_bloginfo('stylesheet_directory').'/js/backstretch-set.js' , array( 'jquery', 'custom-backstretch' ), '1.0.0' );
+
+        //* Load FlexSlider
+        wp_enqueue_script( 'flexslider', get_bloginfo('stylesheet_directory').'/js/flexslider.js' , array( 'jquery'), '1.0.0' );
+        wp_enqueue_script( 'flexslider-set', get_bloginfo('stylesheet_directory').'/js/flexslider-set.js' , array( 'jquery', 'flexslider'), '1.0.0' );
+
+        wp_localize_script( 'custom-backstretch-set', 'BackStretchImg', array( 'src' => get_background_image() ) );
+    }
+
+    //* Add custom background callback for background color
+    function custom_background_callback() {
+
+        if ( ! get_background_color() )
+            return;
+
+        printf( '<style>body { background-color: #%s; }</style>' . "\n", get_background_color() );
+
+    }
+
     add_action( 'init', 'register_menus' );
+    add_action( 'wp_enqueue_scripts', 'custom_enqueue_scripts' );
+
+
+    //* Add support for custom background
+    add_theme_support( 'custom-background', array( 'wp-head-callback' => 'custom_background_callback' ) );
+    //* Add support for Post-Thumbnails
+    add_theme_support( 'post-thumbnails' );
+
+    show_admin_bar( false );
 ?>
