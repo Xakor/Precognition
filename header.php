9ece8e8d2048a27059347b97ce5c95e9b7901548
Xakor <fha.post@gmail.com>
2014-05-04 18:51:28 +0200

    Additional CSS refinements
    Search bar, is no longer a search bar, but a basket bar, like was
    originally intended in PSD

--- header.php
+++ header.php
@@ -7,8 +7,15 @@
 				bloginfo( 'name' );
             ?>
         </title>
+
+        <!-- Stylesheets -->
         <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url');?>/style.css">
         <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url');?>/css/grid12_nogutter.css">
+
+        <!-- Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
+
+        <!-- Wordpress includes -->
         <?php wp_head(); ?>
 
     </head>
@@ -17,8 +24,8 @@
             <a href="<?php get_option ('home'); ?>"><img src="<?php bloginfo('template_url'); ?>/img/logo_temp.png" class="head_banner" title="<?php bloginfo('title'); ?>"></a>
         </div>
         <div class="container_12">
-            <div class="grid_4 red head_search_area">
-                <input type="text" name="searchbar" class="search_bar" placeholder="Search">
+            <div class="grid_4 red head_basket_area">
+                <div class="basket_bar">199,95 kr</div>
             </div>
             <div class="grid_8 red head_menu_area">
                 <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
