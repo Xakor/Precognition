4d2a3b4c16f6334b2c874b6093bb69bbe8ec5800
Sommer <sommerawesome@gmail.com>
2014-05-04 15:55:48 +0200

    The_Loop
    First attempt at The_Loop

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
 <?php get_header(); ?>
+<div class="container_12">
+    <?php
+        if ( have_posts() ) {
+            while ( have_posts() ) {
+                the_post(); ?>
 
+                <div class="grid_8 pull_1">
+                    <div class="grid_8">
+                        <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+                    </div>
+                    <div class="grid_8">
+                        <small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
+                    </div>
+                    <div class="grid_8">
+                        <p><?php the_content(); ?></p>
+                    </div>
+                    <div class="grid_8">
+                        <p class="postmetadata">Posted in <?php the_category(', '); ?></p>
+                    </div>
+                </div>
+
+                <?php
+            } // end while
+        } // end if
+    ?>
+</div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
